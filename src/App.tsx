f8210fd77820c8e826803e4ea9432cd485871a37
masaki.yamakawa <masaki.yamakawa@ulsystems.co.jp>
2021-11-10 09:23:24 +0900

    Add stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Mapbox from './pages/Mapbox';
 import WelcomeReact from './pages/WelcomeReact';
 import './App.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './pages/Map.css';
 
 function App() {
   return (
